14b6b5db0f5543c102235285f00abbae75db39e5
Francisco-BT <pancho10810@gmail.com>
2022-01-14 23:07:14 -0600

    feat: add expensive item component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import ExpenseItem from './componets/ExpenseItem';
+
 function App() {
-  return <div className="App">React App</div>;
+  return (
+    <div className="App">
+      React App
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
 }
 
 export default App;
