136f6f67c0e14a354c3ff7cb8772e612e3b952e4
Francisco-BT <pancho10810@gmail.com>
2022-01-15 01:21:31 -0600

    feat: add props to ExpenseItem component

--- src/componets/ExpenseItem.js
+++ src/componets/ExpenseItem.js
@@ -1,12 +1,20 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">$ {props.amount}</div>
       </div>
     </div>
   );
