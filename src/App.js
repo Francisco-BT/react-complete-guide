1dfbc43bc79256ca95e99ae4e505c8c4bccaa743
Francisco-BT <pancho10810@gmail.com>
2022-01-15 13:59:06 -0600

    feat: add NexExpense component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -25,6 +26,7 @@ function App() {
 
   return (
     <div className="App">
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
