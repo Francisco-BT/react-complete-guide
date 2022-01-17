6c5efc751f2e3432b4a7ec8f6422b402f471e3b9
Francisco-BT <pancho10810@gmail.com>
2022-01-17 15:58:49 -0600

    feat: render conditionally add new expense button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -11,11 +13,27 @@ const NewExpense = (props) => {
     };
 
     props.onAddExpense(expenseData);
+    setIsEditing(false)
+  };
+
+  const cancelHandler = () => {
+    setIsEditing(false);
+  };
+
+  const addNewExpenseHandler = () => {
+    setIsEditing(true);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {isEditing ? (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelHandler}
+        />
+      ) : (
+        <button onClick={addNewExpenseHandler}>Add New Expense</button>
+      )}
     </div>
   );
 };
