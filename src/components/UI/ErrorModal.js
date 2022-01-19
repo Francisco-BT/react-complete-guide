f35844d862896a3103b5603e6c1ba5f3f06a5321
Francisco-BT <pancho10810@gmail.com>
2022-01-18 23:46:47 -0600

    feat: add error modale confirm handler

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -7,16 +7,16 @@ import classes from "./ErrorModal.module.css";
 const ErrorModal = (props) => {
   return (
     <div>
-      <div className={classes.backdrop} />
+      <div className={classes.backdrop} onClick={props.onConfirm} />
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
         </header>
-        <div classes={classes.content}>
+        <div className={classes.content}>
           <p>{props.message}</p>
         </div>
         <footer className={classes.actions}>
-          <Button>Okay</Button>
+          <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </Card>
     </div>
