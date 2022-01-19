f8d4680103b6b9bc035783fb7b04749de4ba9d7d
Francisco-BT <pancho10810@gmail.com>
2022-01-18 21:37:59 -0600

    feat: add Card component

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+const AddUser = () => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age">Age (Years)</label>
+        <input type="number" id="age" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
