817385623f077d88ca6b76e4a1ca835ef3535874
Francisco-BT <pancho10810@gmail.com>
2022-03-20 01:00:20 -0600

    feat: add cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import classes from './Cart.module.css';
+
+const Cart = () => {
+  const carItems = (
+    <ul>
+      {[{ id: '1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {carItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
