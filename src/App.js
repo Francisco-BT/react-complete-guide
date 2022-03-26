fd3319497c37a3117f3a64dcff362ea7e2631d6d
Francisco-BT <pancho10810@gmail.com>
2022-03-26 03:02:46 -0600

    feat: add components structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
 }
---
+++ src/components/Demo/MyParagraph.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyParagraph = (props) => {
+  return <p>{props.children}</p>;
+};
+
+export default MyParagraph;
