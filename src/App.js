b7cbb25bf7034e0a115b1acd08a2df31f2213cfb
franciscobtd <franciscobernabe@drata.com>
2022-03-28 09:32:29 -0600

    feat: add form to create new movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -12,7 +13,9 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(`https://swapi.dev/api/films`);
+      const response = await fetch(
+        "https://react-http-1745a-default-rtdb.firebaseio.com/movies.json"
+      );
 
       if (!response.ok) {
         throw new Error("Somenthing went wrong!");
@@ -36,6 +39,10 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler() {
+    console.log("Add movie");
+  }
+
   let content = <p>Found no movies</p>;
 
   if (movies.length > 0) {
@@ -52,6 +59,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
