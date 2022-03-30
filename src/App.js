6152320f627ffa98316a1c7f17b4e53e685ee366
franciscobtd <franciscobernabe@drata.com>
2022-03-30 00:30:15 -0600

    feat: add form and input components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import SimpleInput from "./components/SimpleInput";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
+    <div className="app">
+      <SimpleInput />
     </div>
   );
 }
