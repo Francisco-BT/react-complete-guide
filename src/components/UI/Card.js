066efd070f6efc99a39f44b5075a30b07284b382
franciscobtd <franciscobernabe@drata.com>
2022-04-06 22:26:31 -0500

    feat: setup project

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,8 +1,13 @@
-import "./Card.css";
+import classes from './Card.module.css';
 
-function Card(props) {
-  const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>;
-}
+const Card = (props) => {
+  return (
+    <section
+      className={`${classes.card} ${props.className ? props.className : ''}`}
+    >
+      {props.children}
+    </section>
+  );
+};
 
 export default Card;
