1e9dcb27ebae31f304b97b09d6b867ce9c1cee87
franciscobtd <franciscobernabe@drata.com>
2022-04-06 22:53:42 -0500

    feat: use redux to hide shopping cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useSelector } from "react-redux";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
 
 function App() {
+  const { cartIsVisible } = useSelector((state) => state.ui);
+
   return (
     <Layout>
-      <Cart />
+      {cartIsVisible && <Cart />}
       <Products />
     </Layout>
   );
