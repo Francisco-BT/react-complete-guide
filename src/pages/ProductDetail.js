597183d40c2f021654921f7a4141c85ecae8679d
franciscobtd <franciscobernabe@drata.com>
2022-04-12 01:09:38 -0500

    feat: extract route params

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,5 +1,15 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
-  return <h1>Product Detail</h1>;
+  const params = useParams();
+  console.log(params.productId);
+
+  return (
+    <section>
+      <h1>Product Detail</h1>
+      <p>{params.productId}</p>
+    </section>
+  );
 };
 
 export default ProductDetail;
