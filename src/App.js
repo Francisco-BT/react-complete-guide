64fb611fd33de9e6953b9caae17bedd839a67a98
franciscobtd <franciscobernabe@drata.com>
2022-04-12 00:43:59 -0500

    feat: add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
---
+++ src/components/MainHeader.js
@@ -0,0 +1,18 @@
+const MainHeader = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/welcome">Welcome</a>
+          </li>
+          <li>
+            <a href="/products">Products</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainHeader;
