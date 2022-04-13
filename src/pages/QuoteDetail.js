16cd863968efe376d8c1a6bf7e058fbe444c9812
franciscobtd <franciscobernabe@drata.com>
2022-04-13 01:02:31 -0500

    feat: show quote detail content

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,33 @@
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Redirect } from "react-router-dom";
+
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "Maximillian",
+    text: "Learning React is great!",
+  },
+];
 
 const QuoteDetails = () => {
   const { quoteId } = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
+
+  if (!quote) {
+    return <p>No quote Found!</p>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path="/quotes/:quoteId/comments">
         <Comments />
       </Route>
