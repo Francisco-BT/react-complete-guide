8caa94c9aacb338a3d0007443faaba11e6b960f1
franciscobtd <franciscobernabe@drata.com>
2022-04-13 20:14:53 -0500

    feat: save new quotes into firebase

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,17 +1,27 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 import QuoteForm from "../components/quotes/QuoteForm";
+import { addQuote } from "../lib/api";
+import useHttp from "../hooks/use-http";
 
 const NewQuote = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
-  const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
 
-    history.push("/quotes");
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default NewQuote;
