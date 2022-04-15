f71f1233172c78f0033a0d5c28e9e16bcbee337a
franciscobtd <franciscobernabe@drata.com>
2022-04-15 01:01:23 -0500

    feat: add auth context to store loggedin state

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,8 +1,10 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const authCtx = useContext(AuthContext);
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const emailInputRef = useRef();
@@ -50,7 +52,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        console.log(data);
+        authCtx.login(data.idToken);
       })
       .catch((err) => {
         console.log("in catch block");
