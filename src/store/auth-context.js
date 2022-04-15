b4ca98be0ec4258831625951b31dce658e5595cf
franciscobtd <franciscobernabe@drata.com>
2022-04-15 02:36:12 -0500

    feat: set auto logout on reload

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+
+let logoutTimer;
 
 const AuthContext = React.createContext({
   token: "",
@@ -15,22 +17,52 @@ const calculateRemainingTime = (expirationTime) => {
   return remainingDuration;
 };
 
+const retrieveStoreToken = () => {
+  const storedToken = localStorage.getItem("token");
+  const storeExpirationDate = localStorage.getItem("expiration");
+
+  const remainigTime = calculateRemainingTime(storeExpirationDate);
+
+  if (remainigTime <= 60000) {
+    localStorage.clear();
+    return {};
+  }
+
+  return {
+    token: storedToken,
+    duration: remainigTime,
+  };
+};
+
 export const AuthContextProvider = (props) => {
-  const initialToken = localStorage.getItem("token");
+  const { token: initialToken, duration } = retrieveStoreToken();
+
   const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
 
-  const logoutHandler = () => {
+  const logoutHandler = useCallback(() => {
     setToken(null);
     localStorage.clear();
-  };
+
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  }, []);
 
   const loginHandler = (token, expirationTime) => {
     setToken(token);
     localStorage.setItem("token", token);
+    localStorage.setItem("expiration", expirationTime);
     const remainigTime = calculateRemainingTime(expirationTime);
-    setTimeout(logoutHandler, remainigTime);
+    logoutTimer = setTimeout(logoutHandler, remainigTime);
   };
+
+  useEffect(() => {
+    if (duration) {
+      console.log("token data duration: ", duration);
+      logoutTimer = setTimeout(logoutHandler, duration);
+    }
+  }, [duration, logoutHandler]);
 
   const contextValue = {
     token,
