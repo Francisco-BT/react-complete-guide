8b1ca5c0951cfbe1b0769cd8fee95a3e5117d4d3
franciscobtd <franciscobernabe@drata.com>
2022-04-16 16:54:31 -0500

    feat: add Output component to test connected components

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import Output from "./Output";
+
 const Greeting = () => {
   const [changeText, setChangeText] = useState(false);
 
@@ -10,8 +12,7 @@ const Greeting = () => {
   return (
     <div>
       <h2>Hello World!</h2>
-      {!changeText && <p>It's good to see you!</p>}
-      {changeText && <p>Changed!</p>}
+      <Output>{changeText ? "Changed!" : "It's good to see you!"}</Output>
       <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
