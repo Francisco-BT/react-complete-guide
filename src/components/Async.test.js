e4c56615c82c53e90467d93b9e3c9e88d631c735
franciscobtd <franciscobernabe@drata.com>
2022-04-16 17:17:08 -0500

    feat: use mocks to simulate async request

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -4,6 +4,11 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("renders post if request success", async () => {
+    window.fetch = jest.fn().mockResolvedValueOnce({
+      json: jest
+        .fn()
+        .mockResolvedValueOnce([{ id: "p1", title: "first post" }]),
+    });
     render(<Async />);
 
     const listItems = await screen.findAllByRole("listitem");
