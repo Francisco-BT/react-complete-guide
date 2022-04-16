f180862420c2abef84114d8dbc742b77411df00f
franciscobtd <franciscobernabe@drata.com>
2022-04-16 02:55:08 -0500

    feat: use react transition group to animate modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,17 +1,41 @@
 import React from "react";
 
+import { Transition } from "react-transition-group";
 import "./Modal.css";
 
-const modal = (props) => {
-  const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClose"];
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
 
+const modal = (props) => {
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
+      {(state) => {
+        const cssClasses = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClose"
+            : "",
+        ];
+
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 
