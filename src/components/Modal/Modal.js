27dd95c41360868341ae855c199e2ee74c34151a
franciscobtd <franciscobernabe@drata.com>
2022-04-16 03:08:45 -0500

    feat: use CSSTransition component to animate modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Transition } from "react-transition-group";
+import { CSSTransition } from "react-transition-group";
 import "./Modal.css";
 
 const animationTiming = {
@@ -10,32 +10,23 @@ const animationTiming = {
 
 const modal = (props) => {
   return (
-    <Transition
+    <CSSTransition
       in={props.show}
       timeout={animationTiming}
       mountOnEnter
       unmountOnExit
-    >
-      {(state) => {
-        const cssClasses = [
-          "Modal",
-          state === "entering"
-            ? "ModalOpen"
-            : state === "exiting"
-            ? "ModalClose"
-            : "",
-        ];
-
-        return (
-          <div className={cssClasses.join(" ")}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
+      classNames={{
+        enterActive: "ModalOpen",
+        exitActive: "ModalClose",
       }}
-    </Transition>
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
