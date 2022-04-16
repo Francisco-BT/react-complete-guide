dc5b47832fd02fcf618d9837e8a26dd3310cbbe5
franciscobtd <franciscobernabe@drata.com>
2022-04-15 23:06:10 -0500

    fix: add empty string for mongo connection

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -3,9 +3,7 @@ import { MongoClient } from "mongodb";
 async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const client = await MongoClient.connect(
-      "mongodb+srv://next:nexttest@cluster0.avszx.mongodb.net/meetups?retryWrites=true&w=majority"
-    );
+    const client = await MongoClient.connect("");
     const db = client.db();
     const meetupsCollection = db.collection("meetups");
     await meetupsCollection.insertOne(data);
