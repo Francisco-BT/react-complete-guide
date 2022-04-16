fd64160aeaf8cfada3a6fbd669bfce66b1e789e1
franciscobtd <franciscobernabe@drata.com>
2022-04-16 16:36:52 -0500

    feat: add state change test for Greeting component

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greetings";
 
 describe("Greeting component", () => {
@@ -12,5 +13,34 @@ describe("Greeting component", () => {
     // Assert
     const helloWorld = screen.getByText("Hello World!");
     expect(helloWorld).toBeInTheDocument();
+  });
+
+  test("renders good to see you if the button was NOT clicked", () => {
+    render(<Greeting />);
+
+    const paragraphEl = screen.getByText("good to see you", { exact: false });
+    expect(paragraphEl).toBeInTheDocument();
+  });
+
+  test("renders Changed! if the button was clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const paragraphElement = screen.getByText("Changed!");
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  test("does not render good to see if the button was clicked", () => {
+    render(<Greeting />);
+
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const paragraphElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(paragraphElement).toBeNull();
   });
 });
--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [changeText, setChangeText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangeText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It's good to see you!</p>
+      {!changeText && <p>It's good to see you!</p>}
+      {changeText && <p>Changed!</p>}
+      <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
 };
