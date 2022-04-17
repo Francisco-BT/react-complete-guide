9ec7c4a50d428d4116c69f09f2adcab25cc58ad7
franciscobtd <franciscobernabe@drata.com>
2022-04-16 22:05:16 -0500

    feat: add logic to remove todos

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 import classes from "./TodoItem.module.css";
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li className={classes.item}>{props.text}</li>;
+const TodoItem: React.FC<{ text: string; onRemove: () => void }> = (props) => {
+  return (
+    <li className={classes.item} onClick={props.onRemove}>
+      {props.text}
+    </li>
+  );
 };
 
 export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -4,11 +4,17 @@ import TodoItem from "./TodoItem";
 import Todo from "../models/todo";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem text={item.text} key={item.id} />
+        <TodoItem
+          text={item.text}
+          key={item.id}
+          onRemove={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
