7cb334883e0c59e0bd65eeca35af066018b45c14
franciscobtd <franciscobernabe@drata.com>
2022-04-16 21:45:02 -0500

    feat: add todos logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import NewTodo from "./components/NewTodo";
 
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+
+const NewTodo: React.FC<{
+  onAddTodo: (text: string) => void;
+}> = (props) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = textInputRef.current!.value;
+
+    if (enteredText?.trim().length === 0) {
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo Text</label>
+      <input type="text" id="text" ref={textInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
