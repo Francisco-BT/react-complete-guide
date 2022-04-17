bae4c113b1e018442d5295cfbdf6edd028e1895c
franciscobtd <franciscobernabe@drata.com>
2022-04-16 20:39:14 -0500

    feat: add a todo data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import "./App.css";
+
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
-  return <div></div>;
+  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+
+  return (
+    <div>
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
